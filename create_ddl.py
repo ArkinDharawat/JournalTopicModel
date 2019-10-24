61993c4fdb719b69fbd07d460288fa09fb85e2f6
ArkinDharawat <arkinrd2@illinois.edu>
2019-10-23 19:54:53 -0500

    Added DDL script

---
+++ create_ddl.py
@@ -0,0 +1,30 @@
+import logging
+import os
+
+import mysql.connector
+import yaml
+from SQLQueries.SQLStrQuery import SQLStrQuery
+
+logger = logging.getLogger("ddl-script")
+
+SQLStrObj = SQLStrQuery(5)  # TODO: Change to global var
+
+# Read YAML file
+with open(os.path.join(os.getcwd(), "JournalTopicModel/config.yml"), 'r') as stream:
+    config = yaml.safe_load(stream)
+
+cnx = mysql.connector.connect(**config)
+logger.info("Connected to SQL")
+
+cursor = cnx.cursor()
+logger.info("Created Cursor")
+
+for ddl_query_str in SQLStrObj.create_tables():
+    cursor.execute(ddl_query_str)
+
+logger.info("Created tables")
+
+cursor.close()
+cnx.close()
+
+logger.info("All Connections Closed")
