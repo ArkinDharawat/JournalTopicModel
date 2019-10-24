d3d07a01de1bb74e889aeb655828f64937a05a0a
ArkinDharawat <arkinrd2@illinois.edu>
2019-10-24 00:13:51 -0500

    Added insert/update/delete

--- SQLQueries/SQLStrQuery.py
+++ SQLQueries/SQLStrQuery.py
@@ -20,12 +20,41 @@ class SQLStrQuery(object):
         return [table_acad_journal, table_acad_paper, table_topic]
 
     def insert_journal(self):
-        return "INSERT INTO Academic_Journal (Journal_Id, Journal_Name, Category) VALUES (%s, %s, %s)"
+        return "INSERT INTO Academic_Journal (Journal_Id, Journal_Name, Category) VALUES (%s, %s, %s);"
 
-    # TODO: INSERT
-    # TODO: UPDATE
+    def insert_paper(self):
+        return "INSERT INTO Academic_Paper (Paper_Id, Authors, Journal_Id, Title, Abstract) VALUES (%s, %s, %s, %s, %s);"
 
+    def update_paper(self, col_names):
+        alter_str = ','.join([x + "=%s" for x in col_names])
+        return "UPDATE Academic_Paper SET " + alter_str + " WHERE Paper_Id = %s;"
+
+    def delete_paper(self):
+        return "DELETE FROM Academic_Paper WHERE Paper_Id = %s;"
+
+    def insert_topic(self, topic_indices):
+        topic_str = ','.join(["Topic{0}".format(i) for i in range(self.num_topics)])
+        value_str = ["0"] * self.num_topics
+        for k in topic_indices:
+            value_str[k - 1] = "1"
+        value_str = ",".join(value_str)
+
+        return "INSERT INTO Topics_per_Paper (Paper_Id, " + topic_str + ") VALUES (%s, " + value_str + ");"
+
+    def update_topic(self, topic_indices):
+        value_str = ["0"] * self.num_topics
+        for k in topic_indices:
+            value_str[k - 1] = "1"
+
+        alter_str = ','.join(["Topic" + str(i) + "=" + value_str[i] for i in range(self.num_topics)])
+
+        return "UPDATE Topics_per_Paper SET " + alter_str + " WHERE Paper_Id = %s;"
+
+    def delete_topic(self):
+        return "DELETE FROM Topics_per_Paper WHERE Paper_Id = %s;"
 
 if __name__ == '__main__':
     obj = SQLStrQuery(5)
-    obj.create_tables()
+    # print(obj.update_paper([2, 4]))
+    # obj.create_tables()
+    # obj.insert_topic([2, 4])
