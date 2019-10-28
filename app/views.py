4cac87895a504089f88ad4222a45ec93f6a24491
Archit Gupta <archit.941@gmail.com>
2019-10-28 14:36:20 -0500

    add templates

--- app/views.py
+++ app/views.py
@@ -1,26 +1,46 @@
 from flask import Flask, request, jsonify
 from app import app
+import mysql.connector
+import yaml
+from SQLQueries.SQLStrQuery import SQLStrQuery
 
+INSERT = "Insert"
+DELETE = "Delete"
+UPDATE = "Update"
+SEARCH = "Search"
+@app.before_first_request
+def before_request_func():
 
 # static url
 @app.route('/')
 def index():
-    return "Hello World!"
+    return render_template('templates/index.html')
 
-@app.route('/insert', methods=['POST'])
+@app.route('/query', methods=['POST'])
 def insert_endpoint():
-    paper_id = request.form['paper_id']
-    authors = request.form['authors']
-    journal_id = request.form['journal_id']
-    title = request.form['title']
-    abstract = request.form['abstract']
-    
+    action = str(request.form.get('action'))
+    if action == INSERT:
+        pass
+    elif action == DELETE:
+        pass
+    elif action == UPDATE:
+        pass
+    elif action == SEARCH:
+        pass
+
+def insert_data(request):
+    paper_id = str(request.form['paper_id'])
+    authors = str(request.form['authors'])
+    title = str(request.form['title'])
+    abstract = str(request.form['abstract'])
+
+def delete_data(request):
+    paper_id = str(request.form['paper_id'])
+
+def update_data(request):
+    paper_id = str(request.form['paper_id'])
 
 
-@app.route('/insert', methods=['POST'])
-def update_endpoint():
+def search_data(request):
     pass
 
-@app.route('/insert', methods=['POST'])
-def delete_endpoint():
-    pass
