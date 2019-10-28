850096f2887642f41ea4d529aa5c71bcca002472
Archit Gupta <archit.941@gmail.com>
2019-10-28 15:11:40 -0500

    a

--- app/views.py
+++ app/views.py
@@ -11,6 +11,7 @@ SEARCH = "Search"
 cnx = None
 SQLStrObj = None
 TopicModelobj = None
+cursor = None
 @app.before_first_request
 def before_request_func():
     SQLStrObj = SQLStrQuery(10)
@@ -18,6 +19,7 @@ def before_request_func():
         config = yaml.safe_load(stream)
     cnx = mysql.connector.connect(**config)
     TopicModelobj = TopicModel(os.path.join(os.path.expanduser('~'), "../../project/data/"))
+    cursor = cnx.cursor()
 # static url
 @app.route('/')
 def index():
@@ -41,7 +43,6 @@ def insert_data(request):
     title = str(request.form['title'])
     abstract = str(request.form['abstract'])
     journal_id = str(request.form['journal_id'])
-    cursor = cnx.cursor()
     insert_paper_query = SQLStrObj.insert_paper()
     topics = TopicModelobj.get_topics(title=title, abstract=abstract)
     insert_topic_query = SQLStrObj.insert_topic(paper_id, topics)
@@ -50,15 +51,50 @@ def insert_data(request):
         cursor.execute(insert_topic_query)
     except Exception as e:
         pass  
-    cursor.close()
 
 def delete_data(request):
     paper_id = str(request.form['paper_id'])
+    delete_paper_query = SQLStrObj.delete_paper()
+    try:
+        cursor.execute(delete_paper_query, paper_id)
+    except Exception as e:
+        pass
+def filter_update_data(request):
+    default = "Default"
+    column = []
+    data = []
 
+    authors = str(request.form['authors']) 
+    if authors != default: 
+        column.append("authors")
+        data.append(authors)
+    title = str(request.form['title'])
+    if title != default: 
+        column.append("title")
+        data.append(title)
+    abstract = str(request.form['abstract'])
+    if abstract != default: 
+        column.append("abstract")
+        data.append(abstract)
+    journal_id = str(request.form['journal_id'])
+    if journal_id != default: 
+        column.append("journal_id")
+        data.append(journal_id)
+    return column, data
 def update_data(request):
     paper_id = str(request.form['paper_id'])
-
+    column, data = filter_update_data(request)
+    if column != [] and data != []:
+        data.append(paper_id)
+        data = tuple(data)
+        update_query = SQLStrObj.update_paper
+        try:
+            cursor.execute(update_query, data)
+        except Exception as e:
+            pass
 
 def search_data(request):
     pass
+
+
 
