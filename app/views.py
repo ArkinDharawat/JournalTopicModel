d07972b783a3b9fcfb875704eb20b4864d1e5aaf
ArkinDharawat <arkinrd2@illinois.edu>
2019-10-28 20:21:35 -0500

    Print values neatly

--- app/views.py
+++ app/views.py
@@ -162,9 +162,10 @@ def search_data(request):
         search_paper = SQLStrObj.search_paper()
         try:
             cursor.execute(search_paper, (paper_id,))
+            ret_str = ','.join(cursor.column_names) + "\n"
             for row in cursor:
-                print(row)
-            return ""
+                ret_str = ret_str + ','.join(list(row)) + "\n"
+            return ret_str
         except Exception as e:
             return "Error :" + str(e)
 
