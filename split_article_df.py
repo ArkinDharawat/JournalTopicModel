78371e875d3a30ac0bc766313ced3fa1f096b664
ArkinDharawat <arkinrd2@illinois.edu>
2019-11-13 15:45:25 -0600

    Added print statements

--- split_article_df.py
+++ split_article_df.py
@@ -11,11 +11,11 @@ journal_df = pd.read_csv("/home/project/data/journalslist.csv")
 
 field_count = defaultdict(lambda : 0)
 
-logger.info("datasets loaded")
+print("datasets loaded")
 
 i = 0
 for chunk in df_full:
-    logger.info("At chunk: {0}".format(i))
+    print("At chunk: {0}".format(i))
     df_chunked = chunk.iloc[:, [1, 2, 9, 10, 13]]
     df_chunked.columns = ["title", "author", "url", "abstract", "journal_id"]
     for id, row in df_chunked.iterrows():
@@ -23,7 +23,7 @@ for chunk in df_full:
         for t in topics:
             field_count[t] += 1
     i += 1
-logger.info("Sorting and Printing")
+print("Sorting and Printing")
 import operator
 sorted_x = sorted(field_count.items(), key=operator.itemgetter(1))
 for x in sorted_x:
