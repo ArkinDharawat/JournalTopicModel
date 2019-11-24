ad67a147e6334785a14f020e70db1d2ab1d5b0e9
ArkinDharawat <arkinrd2@illinois.edu>
2019-11-24 13:37:43 -0800

    Addeed method to create nodes

--- create_neo4j.py
+++ create_neo4j.py
@@ -2,16 +2,31 @@ import os
 
 import pandas as pd
 import yaml
-from py2neo import Graph,  Node, Relationship
+from py2neo import Graph, Node, Relationship
 
 with open(os.path.join(os.getcwd(), "config_neo.yml"), 'r') as stream:
     config = yaml.safe_load(stream)
 
 graph = Graph(auth=(config["user"], config["password"]))
 
-journal_df = pd.read_csv(os.path.join(os.path.expanduser('~'), "../project/data/journalslist.csv"))
-papers_df = pd.read_csv(os.path.join(os.path.expanduser('~'), "../project/data/AllArticles_Subset.csv"))
 
+def add_journal_nodes():
+    journal_df = pd.read_csv(os.path.join(os.path.expanduser('~'), "../project/data/journalslist.csv"))
+    journal_rankings = pd.read_csv(os.path.join(os.path.expanduser('~'), "../project/data/JournalIdRankings.csv"))
+
+    for id, row in journal_df.iterrows():
+        name, id, field = row.values
+        ranking = journal_rankings[journal_rankings["journal_id"] == id]["ranking"].values
+        if len(ranking) == 0:
+            pass
+        else:
+            ranking = ranking[0]
+        print(name, id, field, ranking)
+
+
+def add_paper_nodes():
+    pass
+
+add_journal_nodes()
 import code
 code.interact(local={**locals(), **globals()})
-
