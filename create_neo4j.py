29188b5f7470058fa3b2110453295085e407096d
ArkinDharawat <arkinrd2@illinois.edu>
2019-11-24 13:16:36 -0800

    Added full paths

--- create_neo4j.py
+++ create_neo4j.py
@@ -7,12 +7,10 @@ from py2neo import Graph,  Node, Relationship
 with open(os.path.join(os.getcwd(), "config_neo.yml"), 'r') as stream:
     config = yaml.safe_load(stream)
 
-model_folder = "/Users/arkin/Desktop/CS411/project/data"
-
 graph = Graph(auth=(config["user"], config["password"]))
 
-journal_df = pd.read_csv(os.path.join(model_folder, "journalslist.csv"))
-papers_df = pd.read_csv(os.path.join(model_folder, "AllArticles_Subset.csv"))
+journal_df = pd.read_csv(os.path.join(os.path.expanduser('~'), "../project/data/journalslist.csv"))
+papers_df = pd.read_csv(os.path.join(os.path.expanduser('~'), "../project/data/AllArticles_Subset.csv"))
 
 import code
 code.interact(local={**locals(), **globals()})
