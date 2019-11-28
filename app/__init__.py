eff92e918f954d16ca3bcdd139bbbf01404b1273
ArkinDharawat <arkinrd2@illinois.edu>
2019-11-28 00:15:22 -0800

    Added config file

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,5 @@
 import click
 from flask import Flask
 app = Flask(__name__)
-@app.cli.command('set-db')
-@click.argument("db_type")
-def set_db(db_type):
-    print(db_type)
-    from app import views
+app.config.from_object('config')
+from app import views
