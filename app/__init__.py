69555b400b1a7078f935b3e7d03e10d323e1b5e4
ArkinDharawat <arkinrd2@illinois.edu>
2019-11-27 23:32:15 -0800

    Test arg with argparse

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,9 @@
-from argparse import ArgumentParser
-
 from flask import Flask
+# from argparse import ArgumentParser
 
-parser = ArgumentParser()
-parser.add_argument('-db_type', default="sql")
-args = parser.parse_args()
+# parser = ArgumentParser()
+# parser.add_argument('-db_type', default="sql")
+# args = parser.parse_args()
 
 app = Flask(__name__)
-if args.db_type not in ["sql", "neo4j"]:
-    args.db_type = "sql"
-app.config['database'] = args.db_type
+from app import views
