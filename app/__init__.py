23e3c968610e6fe33d17ebc3100c598e64c48193
ArkinDharawat <arkinrd2@illinois.edu>
2019-11-27 23:51:24 -0800

    Added sys arguments

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ import sys
 
 app = Flask(__name__)
 from app import views
-if len(sys.argv) < 3:
+if len(sys.argv) < 4:
     app.config['db_type'] = "sql"
 elif sys.argv[3] not in ["sql", "neo"]:
     app.config['db_type'] = "sql"
